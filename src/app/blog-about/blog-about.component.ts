c6561973d8538111e9ce1f96b18ce6d008768215
DavidDavydov07 <david.davydov07@students.kbcc.cuny.edu>
2018-12-01 05:45:49 -0500

    Fixed the about router not redirecting correctly. Added the About page.

--- src/app/blog-about/blog-about.component.ts
+++ src/app/blog-about/blog-about.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog-about.component.css']
 })
 export class BlogAboutComponent implements OnInit {
-
+  public windowHeight;
   constructor() { }
 
   ngOnInit() {
+    let orientationEvent = "onorientationchange" in window ? "orientationchange" : "resize";
+    
+    this.windowHeight = `${document.documentElement.clientHeight - 79.13}px`;
+
+    window.addEventListener(orientationEvent, function() {
+
+      if(window.orientation === 0 || window.orientation === 180){
+        this.windowHeight = `${document.documentElement.clientHeight - 79.13}px`;
+        console.log(window.orientation + " " + this.windowHeight)
+      }
+      else{
+        this.windowHeight = `${document.documentElement.clientHeight - 79.13}px`;
+        console.log(window.orientation + " " + this.windowHeight)
+      }
+    }, false);
   }
 
 }
